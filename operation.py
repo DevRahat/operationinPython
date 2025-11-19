6c34d4124e18998733774d122df7d7124b5fcc2e
DevRahat <dev.rahatislam@gmail.com>
2025-11-19 19:51:37 +0600

    Python operators

--- operation.py
+++ operation.py
@@ -1,8 +1,26 @@
 a=60000
 b=60000
-
 print(b is a) 
 
 a="Bangladesh"
 b="desh"
 print(b in a) 
+
+# += operator in python 
+a=5
+a+=2
+print(a)
+
+# -= operator in python 
+a-=2
+print(a)
+
+# *= operator in python 
+a*=2
+print(a)
+
+# /= operator in python 
+a/=2
+print(a)
+
+# %= operator in python 
