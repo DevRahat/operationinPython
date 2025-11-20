843c5b0b9eb0e4f79826c39a1e0a58630b410f69
DevRahat <dev.rahatislam@gmail.com>
2025-11-20 19:22:41 +0600

    string

--- text.py
+++ text.py
@@ -5,6 +5,10 @@ print("I am Rahat Islam, my Motherland is \"Bangladesh\"")
 #print with another format
 print("I am Rahat Islam, my Motherland is %s" % "Bangladesh")
 
+#string formating 
+name = "Rahat Islam"
+print("My name is:", name)
+
 
 
 
