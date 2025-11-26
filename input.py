f53e456f74e457add95aa0da2abf49a75299b619
DevRahat <dev.rahatislam@gmail.com>
2025-11-26 19:35:23 +0600

    26th november

--- input.py
+++ input.py
@@ -2,3 +2,4 @@ a=input()
 b=input()
 
 print('My fav language is: %s and %s' %(a,b))
+
---
+++ string.py
@@ -0,0 +1,7 @@
+a="bangla"
+b="India"
+
+print(f"{a} and {b} are neighbors")
+
+x=input("Enter your name: ")
+print("MY name is:%s" %x)
