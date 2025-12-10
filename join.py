0b6e19bec47b97fbc4d9085ace0886da2bae690d
DevRahat <dev.rahatislam@gmail.com>
2025-12-10 17:39:33 +0600

    join function

---
+++ join.py
@@ -0,0 +1,18 @@
+a="Dhaka"
+b="Rangpur"
+c="Jessore"
+
+print(a.capitalize())
+print(f"{a}  {b} and {c} are my favorite cities")
+print(a.upper())
+print(f"{a}  {b} and {c} are my favorite cities")
+
+
+print("-" .join([a,b,c]))
+
+
+
+
+
+
+
